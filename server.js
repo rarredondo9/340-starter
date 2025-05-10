407ddb229c1191723560b69d0b41a331b6750e8b
rarredondo9 <rebeca.arredondo.9@gmail.com>
2025-05-10 13:21:16 -0700

    initial commit

--- server.js
+++ server.js
@@ -8,12 +8,27 @@
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
+const expressLayouts = require("express-ejs-layouts")
 const static = require("./routes/static")
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+//Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
