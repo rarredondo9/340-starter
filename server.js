ae6111fac9d873ac4febcae785d5b1bd02d2a6c2
rarredondo9 <rebeca.arredondo.9@gmail.com>
2025-05-10 14:08:34 -0700

    assignment 1

--- server.js
+++ server.js
@@ -9,40 +9,40 @@
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
+const session = require("express-session")
+const flash = require("connect-flash")
+
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("./utilities")
-const session = require("express-session")
-const pool = require('./database/')
-const accountRoute = require("./routes/accountRoute")
 
 /* ***********************
  * View Engine Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") 
-
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Middleware
- * ************************/
+ *************************/
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }))
+
+// Session middleware
 app.use(session({
-  store: new (require('connect-pg-simple')(session))({
-    createTableIfMissing: true,
-    pool,
-  }),
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || "secret", // fallback if .env is missing
   resave: true,
   saveUninitialized: true,
-  name: 'sessionId',
+  name: 'sessionId'
 }))
 
-// Express Messages Middleware
-app.use(require('connect-flash')())
-app.use(function(req, res, next){
+// Flash messages middleware
+app.use(flash())
+
+// Middleware to expose flash messages to views
+app.use((req, res, next) => {
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
@@ -57,36 +57,28 @@ app.use(static)
  *************************/
 app.get("/", baseController.buildHome)
 
-// Inventory routes
-app.use("/inv", inventoryRoute)
-
-// Account Route
-app.use("/account", accountRoute)
 /* ***********************
  * Error handling middleware
  *************************/
 app.use((err, req, res, next) => {
-  console.error(err.stack); 
-  res.status(err.status || 500);
-
+  console.error(err.stack)
+  res.status(err.status || 500)
   res.send(`
     <h1>Server Error</h1>
     <p><strong>${err.message}</strong></p>
     <p>${err.stack}</p>
-  `);
-});
- 
+  `)
+})
 
 /* ***********************
  * Local Server Information
- * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000
+const host = process.env.HOST || "localhost"
 
 /* ***********************
- * Log statement to confirm server operation
+ * Start Server
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`App listening on http://${host}:${port}`)
 })
--- utilities/index.js
+++ utilities/index.js
@@ -1,29 +1,17 @@
-const invModel = require("../models/inventory-model");
-
 const Util = {};
 
 /* ************************
  * Constructs the nav HTML unordered list
  ************************** */
 Util.getNav = async function () {
-  let data = await invModel.getClassifications();
-  let list = "<ul>";
-  list += '<li><a href="/" title="Home page">Home</a></li>';
-  data.rows.forEach((row) => {
-    list += "<li>";
-    list +=
-      '<a href="/inv/type/' +
-      row.classification_id +
-      '" title="See our inventory of ' +
-      row.classification_name +
-      ' vehicles">' +
-      row.classification_name +
-      "</a>";
-    list += "</li>";
-  });
-  list += "</ul>";
-  return list;
-};
+  return `
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="#">Vehicles</a></li>
+      <li><a href="#">About</a></li>
+    </ul>
+  `
+}
 
 /* **************************************
  * Build the classification view HTML
