8a54e026c7798ab0125373f82954aebdb9344157
rarredondo9 <rebeca.arredondo.9@gmail.com>
2025-06-05 13:49:14 -0700

    register form

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,5 +4,9 @@ const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+router.post("/register", utilities.handleErrors(accountController.registerAccount))
+
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -17,13 +17,14 @@ const pool = require("./database/")
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const accountRoute = require("./routes/accountRoute") 
 const utilities = require("./utilities")
-const accountRoute = require("./routes/accountRoute") // Add this line
-
 
 const app = express()
 
-// Session middleware
+/* ************************
+ * Session Middleware
+ ************************ */
 app.use(session({
   store: new pgSession({
     pool,
@@ -35,33 +36,46 @@ app.use(session({
   name: 'sessionId',
 }))
 
-// Flash middleware
+/* ***********************
+ * Body Parsing Middleware
+ ************************ */
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+/* ***********************
+ * Flash Message Middleware
+ ************************ */
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.messages = require("express-messages")(req, res)
   next()
 })
 
-// View engine
+/* ***********************
+ * View Engine and Layout
+ ************************ */
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
 
-// Body parser
-app.use(express.urlencoded({ extended: true }))
-
-// Routes
+/* ***********************
+ * Static Route Middleware
+ ************************ */
 app.use(static)
 app.use("/inv", inventoryRoute)
 app.use("/account", accountRoute)
 app.get("/", utilities.handleErrors(baseController.buildHome))
 
-// 404 handler
+/* ***********************
+ * 404 Handler
+ ************************ */
 app.use((req, res, next) => {
   next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
 })
 
-// Error handler
+/* ***********************
+ * Global Error Handler
+ ************************ */
 app.use(async (err, req, res, next) => {
   let nav = ''
   try {
@@ -80,7 +94,11 @@ app.use(async (err, req, res, next) => {
   })
 })
 
+/* ***********************
+ * Start Server
+ ************************ */
 const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
+
